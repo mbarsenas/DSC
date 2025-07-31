3735accb99053f3cb2889436d10ae6dfbc7d2388
SpursFan20mb <127122184+mbarsenas@users.noreply.github.com>
2025-07-30 22:52:08 -0500

    Add files via upload

---
+++ VM-Service.ps1
@@ -0,0 +1,13 @@
+﻿Configuration ServiceTest {
+    Node $PC {
+
+        Service 'VMware Horizon Client' {
+            Name = "client_service"
+            State = "Stopped"
+            StartupType = "Manual"
+        }
+    }
+}
+$PC = "DESKTOP-HEF1AAF"
+ServiceTest -OutputPath c:\dsc\out\ServiceTest
+Start-DscConfiguration -Path c:\dsc\out\ServiceTest -Wait -Verbose -Force
